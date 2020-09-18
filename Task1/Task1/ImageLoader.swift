09ce7399595d31729548ba9ee3f42774620816ae
lisa <violetcactusonline@gmail.com>
2020-09-18 18:42:03 +0400

    Task 1 was fixed

--- Task1/Task1/ImageLoader.swift
+++ Task1/Task1/ImageLoader.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
+typealias OnComplete = (_ image: UIImage) -> Void
+
 class ImageLoader: NSObject {
-    static func loadImage(url: String, onComplete: @escaping (_ image: UIImage) -> Void, onError: @escaping (_ error: Error) -> Void) {
+    static func loadImage(url: String, onComplete: @escaping OnComplete) {
         guard let imageURL = URL(string: url) else { return }
 
         // Create dataTask
         // just not to cause a deadlock in UI!
         let task = URLSession.shared.dataTask(with: imageURL) { data, response, error in
-            guard let data = data, error == nil else { onError(error!); return }
+            guard let data = data, error == nil else { return }
             let image = UIImage(data: data)
 
             DispatchQueue.main.async() {
